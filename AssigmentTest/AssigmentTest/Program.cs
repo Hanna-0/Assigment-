c1d92a39fbd88723988de59e3637ca9527dd64c4
Hanna-0 <delahana.amadhea@gmail.com>
2023-01-20 15:42:08 +0700

    Test PT Elektrindo Inti Dinamika

---
+++ AssigmentTest/AssigmentTest/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace AssigmentTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Begin:
+            #region
+
+            Console.Clear();
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("---------DAFTAR SOAL--------- \n");
+            Console.WriteLine("-------------------------------------- \n" +
+            "Soal no. 1(1) \n" +
+            "Soal no. 2(2) \n");
+            Console.Write("Masukan Nomer Soal : ");
+            int exam = Convert.ToInt16(Console.ReadLine());
+
+            if (exam == 1)
+            {
+                Exam001();
+            }
+            else if (exam == 2)
+            {
+                Exam002();
+            }
+
+            #endregion
+
+            #region Restart Program
+            Console.WriteLine("\nApakah anda ingin merunning program? (y / n)");
+            string ans = Console.ReadLine().ToLower();
+
+            if (ans.Equals("y"))
+            {
+                goto Begin;
+            }
+            else
+            {
+                Environment.Exit(1);
+            }
+
+            #endregion
+        }
+            public static void Exam001()
+            {
+            Console.Write("Masukan Input \t: ");
+           
+            bool prima = true;
+
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if (input >= 2)
+            {
+                for (int i = input; i >= 2; i--)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if ((i % j) == 0)
+                        {
+                            prima = false;
+                            break;
+                        }
+                    }
+
+                    if (prima)
+                    Console.WriteLine("Bilangan " + i + " adalah bilangan prima");
+                    prima = true;
+                }
+            }
+        }
+
+        public static void Exam002()
+        {
+            int a, b, c,d;
+            Console.Write("Masukan Input \t: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            for (b=1; b<=a;b++)
+            {
+                for (d=1; d<=a-b; d++)
+                {
+                    Console.Write(" ");
+                }
+                for (c = 1; c <= b; c++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ CreateDeleteAPI/CreateDeleteAPI/Controllers/DepartementController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateDeleteAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartementController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<Department> InsertDepartment(DepartmentModel model)
+        {
+                DBContext.Departments.Add(model);
+                await DBContext.SaveChangesAsync();
+                return model;
+        }
+
+        [HttpDelete]
+        public bool DeleteDepartment(int ID)
+        {
+            bool result = false;
+            var department = DBContext.Department(ID);
+            if (department != null)
+            {
+                DBContext.Entry(department).State = EntityState.Deleted;
+                DBContext.SaveChanges();
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
